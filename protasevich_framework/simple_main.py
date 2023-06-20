f9a4b04ed20b231855d59322da1e6d6ff6aa896d
dima protasevich <dima_protasevich92@mail.ru>
2023-06-20 11:12:44 +0300

    lesson_2

--- protasevich_framework/simple_main.py
+++ protasevich_framework/simple_main.py
@@ -1,3 +1,6 @@
+from protasevich_framework.my_requests import process_get_data, process_form_data
+
+
 class NotFoundPage:
     def __call__(self, request):
         return '404 Not Found', 'Page not found'
@@ -11,7 +14,7 @@ class Aplication:
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
-        if not path.endswith('/'): #обработка наличия (отсутствия) слеша в конце адрес
+        if not path.endswith('/'):  # обработка наличия (отсутствия) слеша в конце адрес
             path += '/'
         if path in self.routes:
             view = self.routes[path]
@@ -20,7 +23,10 @@ class Aplication:
         request = {}
         for front in self.fronts:
             front(request)
+        if environ['REQUEST_METHOD'] == 'POST':
+            process_form_data(environ)
+        elif environ['REQUEST_METHOD'] == 'GET':
+            process_get_data(environ)
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
-
